ab10f350d735c82ceb2a5c4d7648195ffdac193d
Manuel Utsunomiya Bonisoli <43104858+Manukineko@users.noreply.github.com>
2025-04-10 20:50:59 +0900

    Update docs/config.js

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,8 @@
 const config = {
     otherVersions:["Beta-1"],
-  themeColor:"#FFFFFF",
-  name:"Cupidon - Wiki",
-  description:"The documentation for Cupidon. A small library for creating easy parabola for GameMaker",
   favicon:"assets/docsIcon.png",
+  name:"Cupidon - Wiki",
+  themeColor:"#FFFFFF",
+  description:"The documentation for Cupidon. A small library for creating easy parabola for GameMaker",
   latestVersion:"Beta-1",
 };
